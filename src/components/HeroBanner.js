e5813afd94bbf3ada880007491306671423abbfa
manish257 <manishmahato257@gmail.com>
2022-06-22 11:51:18 +0500

    parts of herobanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
 const HeroBanner = () => {
   return (
-    <div>HeroBanner</div>
+    <Box sx={{
+        mt: { lg: '212px', xs:'70px' },
+        ml: { sm: '50px' }
+    }} position="relative" p="20px">
+        <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+            Fitness Club
+        </Typography>
+        <Typography>
+            Sweat, Smile <br /> and Repeat
+        </Typography>
+    </Box>
   )
 };
 
