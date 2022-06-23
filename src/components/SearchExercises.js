b90f93315fe50323c0aa85782a8caee83d11b5d5
manish257 <manishmahato257@gmail.com>
2022-06-23 16:24:36 +0500

    parts of searchexercises

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+
 const SearchExercises = () => {
   const [search, setSearch] = useState("");
 
   const handleSearch = async () => {
     if(search) {
-        //const exerciseData = await fetchData();
+        const exerciseData = await fetchData()
+        ('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+        console.log(exerciseData);
     }
   }
-
+ 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
       <Typography
